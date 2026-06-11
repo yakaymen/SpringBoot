c02e0b92a9f861c9f9d6b5ef650fe26b19d69ed5
Iván Rodríguez <fullstackinserta2025@gmail.com>
2026-06-11 11:54:16 +0100

    20260611 Creacion del proyecto

---
+++ crudalumnos/src/main/java/com/inserta/crudalumnos/CrudalumnosApplication.java
@@ -0,0 +1,13 @@
+package com.inserta.crudalumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudalumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudalumnosApplication.class, args);
+	}
+
+}
---
+++ crudalumnos/src/test/java/com/inserta/crudalumnos/CrudalumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inserta.crudalumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudalumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
