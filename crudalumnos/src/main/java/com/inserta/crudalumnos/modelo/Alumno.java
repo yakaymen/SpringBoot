9ec44b2143c19df43ed7c720934eae1383617b4a
Iván Rodríguez <fullstackinserta2025@gmail.com>
2026-06-15 09:35:02 +0100

    20260615 Modelo

---
+++ crudalumnos/src/main/java/com/inserta/crudalumnos/modelo/Alumno.java
@@ -0,0 +1,18 @@
+package com.inserta.crudalumnos.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="alumnos")
+public class Alumno {
+    @Id
+    private String nif;
+}
